f0fcb20a2d1037db21d57283ddff017de7e9ffff
ltran <longdtranw@gmail.com>
2022-03-08 00:13:57 -0700

    create the basic visual elements of the menu UI

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+   GameObject MainMenu;
+   GameObject CreateMenu;
+   GameObject CreateCardMenu;
+   GameObject CategoryColorMenu;
+   GameObject CategoryTypeMenu;
+
+   public void TransitionFromMainToCreate() {
+       
+   }
+
+}
