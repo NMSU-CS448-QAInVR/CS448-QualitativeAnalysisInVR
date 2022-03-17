561cc5835764d94d7d8628421b96d696bcb84ef8
Santiago Flores <santiflo2700@gmail.com>
2022-03-16 18:57:46 -0600

    Card text generated from ImportCSV.cs
    Notecard Text Edit script disabled from Notecard prefab and text is now generated from ImportCSV script

--- Assets/Scripts/ImportCSV.cs
+++ Assets/Scripts/ImportCSV.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System;
+using TMPro;
 
 public class ImportCSV : MonoBehaviour
 {
+    private TextMeshPro textEdit;//textmeshpro object
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,19 @@ public class ImportCSV : MonoBehaviour
         string csvFile = System.IO.File.ReadAllText(path); //reads all the text in the file
         string[] data = csvFile.Split(',','\n'); //split strings by commas and newlines
 
-        foreach(string d in data)
+        //Edit the text component of the card using TextMeshPro
+        textEdit = this.GetComponentInChildren<TextMeshPro>();
+        if(textEdit == null)
         {
-            Debug.Log(d);
+            Debug.LogError("Notecard Object does not have the text component");
         }
+
+        this.textEdit.SetText("Lets see how long this text can go before getting cut off by the card It cuts off here");
+
+        //foreach(string d in data)
+        //{
+        //   Debug.Log(d);
+        //}
 
     }
 
