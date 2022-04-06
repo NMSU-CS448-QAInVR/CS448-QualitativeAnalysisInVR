bd18a24ec7eb10ff35a491ea46762ade880f76de
Santiago Flores <santiflo2700@gmail.com>
2022-04-05 18:00:35 -0600

    Changed note cards
    The notecards now have there own layer so that they don't collide with each other. The text also auto sizes on the cards to fit the card

--- Assets/Scripts/ImportCSV.cs
+++ Assets/Scripts/ImportCSV.cs
@@ -18,30 +18,24 @@ public class ImportCSV : MonoBehaviour
             return;
         }
         string csvFile = System.IO.File.ReadAllText(path); //reads all the text in the file
-        string[] data = csvFile.Split(',','\n'); //split strings by commas and newlines
+        string[] data = csvFile.Split(','); //split strings by commas and newlines
 
-        //Edit the text component of the card using TextMeshPro
-        //textEdit = notecard.GetComponentInChildren<TextMeshPro>();
-        //if(textEdit == null)
-        //{
-        //    Debug.LogError("Notecard Object does not have the text component");
-        //}
 
         //this.textEdit.SetText("Lets see how long this text can go before getting cut off it can fit a lot more with the text size being reduced by half");
         float i = 0;
+        Debug.Log("Generate Cards from CSV");
         foreach (string d in data)
         {
             Instantiate(notecard, new Vector3(0, 1, i), Quaternion.Euler(0, 0, 0));
             notecard.GetComponentInChildren<TextMeshPro>().SetText(d);
-            Debug.Log(d);
             i = i + (float)0.01;
+            Debug.Log(d);
         }
 
     }
 
     public void Clicked()
     {
-        Debug.Log("Generate Cards from CSV");
         ReadCSV();
     }
 
