e0e29a355fcb34ce3890fe56ffa75caa87dc0daf
Santiago Flores <santiflo2700@gmail.com>
2022-04-07 20:06:16 -0600

    Update ImportCSV.cs
    Cards now spawn in one at a time per button press

--- Assets/Scripts/ImportCSV.cs
+++ Assets/Scripts/ImportCSV.cs
@@ -4,39 +4,42 @@ using TMPro;
 
 public class ImportCSV : MonoBehaviour
 {
-    private TextMeshPro textEdit;//textmeshpro object
     public GameObject notecard;
-    //public Object note;
-    // Start is called before the first frame update
+    private TextMeshPro textEdit;//textmeshpro object
+    private static int i = 0; //keeps track of what value is being read from csv
+    private static string[] data; //holds parsed strings from csv
 
-    void ReadCSV()
+
+    // Start is called before the first frame update
+    void Start()
     {
+        //try to read in file and save data to string array 'data' before clicked
         string path = (Application.dataPath + "/Other/testFile.csv");
-        if( !(System.IO.File.Exists(path)) )
+        if (!(System.IO.File.Exists(path)))
         {
             Debug.LogError("File could not be found.");
             return;
         }
-        string csvFile = System.IO.File.ReadAllText(path); //reads all the text in the file
-        string[] data = csvFile.Split(','); //split strings by commas and newlines
-
-
-        //this.textEdit.SetText("Lets see how long this text can go before getting cut off it can fit a lot more with the text size being reduced by half");
-        float i = 0;
-        Debug.Log("Generate Cards from CSV");
-        foreach (string d in data)
-        {
-            Instantiate(notecard, new Vector3(0, 1, i), Quaternion.Euler(0, 0, 0));
-            notecard.GetComponentInChildren<TextMeshPro>().SetText(d);
-            i = i + (float)0.01;
-            Debug.Log(d);
-        }
-
+        string csvFile = System.IO.File.ReadAllText(path); //reads all the text in the file as one long string
+        data = csvFile.Split(','); //split string by commas
+        //Debug.Log("Data length: " + data.Length);
     }
 
     public void Clicked()
     {
-        ReadCSV();
+        //check to see if all values have been printed
+        if (i >= data.Length)
+        {
+            Debug.Log("All cards printed do nothing");
+        }
+        else
+        {
+            Debug.Log("Generate card number " + i + " from CSV");
+            Debug.Log(data[i]);
+            Instantiate(notecard, new Vector3(0, 1, 0), Quaternion.Euler(0, 0, 0));//create note card object
+            notecard.GetComponentInChildren<TextMeshPro>().SetText(data[i]); //set text on child component, TextMeshPro, of Notecard object
+            i++;
+        }
     }
 
 }
