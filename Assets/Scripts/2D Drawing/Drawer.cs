46362f722c7a8c0455b2300e36e6497dba0c06a1
Fidel Soto <fidelsotostem@gmail.com>
2022-05-06 02:14:00 -0600

    documentation changes

--- Assets/Scripts/2D Drawing/Drawer.cs
+++ Assets/Scripts/2D Drawing/Drawer.cs
@@ -7,6 +7,13 @@
  * are either on the "Drawboard" or "Notecard" Layers by applying pixels to the texture of 
  * the board.
  * 
+ * When the pen detects that a raycast has hit a drawable object (either a notecard or a board), 
+ * then the pen will shot out a laser letting the user know where the drawing/erasing will land. 
+ * 
+ * When holding the pen, the user will press B to draw, and A to delete.
+ * 
+ * Erasing, in actuality, is just drawing the color of the board. (default to white)
+ * 
  */
 
 using System.Collections;
@@ -27,20 +34,25 @@ public class Drawer : MonoBehaviour
     // If on draw mode, the pen will be drawing red pixels
     public InputActionReference drawMode;
 
+    // The tip that we'll be tracking. The tip will shoot out the raycast and the laser.
     [SerializeField]
     private Transform tip;
     private float tipHeight;
 
+    // How big the drawings should be
     [SerializeField]
     private int penSize = 5;
 
-
+    // Necessary to render the material
     private Renderer r;
     private Color[] colors;
 
+    // Information about the touch on the whiteboard when the raycast is hit
     private RaycastHit touch;
     private Drawable whiteboard;
     
+    // Coordinates of the current position, and previous position if we were drawing in the previous
+    // frame
     private Vector2 touchPos, lastTouchPos;
     private bool touchedLastFrame;
     private Quaternion lastTouchRot;
@@ -66,7 +78,8 @@ public class Drawer : MonoBehaviour
 
     private void Draw()
     {
-
+        // If the raycast hit a notecard or a board, then draw a line to that drawable object and if the user is pressing A or B, 
+        // then go ahead and draw or delete.
         if ((Physics.Raycast(tip.position, transform.right, out touch, Mathf.Infinity, LayerMask.GetMask("NoteCard"))
                 || Physics.Raycast(tip.position, transform.right, out touch, Mathf.Infinity, LayerMask.GetMask("DrawBoard")))
             && touch.transform.CompareTag("Drawable") && GetComponent<DrawController3D>().trigger.action.ReadValue<float>() == 0.0f)
@@ -96,7 +109,7 @@ public class Drawer : MonoBehaviour
             // Notecards have a BoxCollider and a MeshCollider. 
             // Refactoring them such that their prefabs match the board prefabs
             // was too complex, so for now I'm opting to just toggle the box collider
-            // of the notecard when drawing is happening.
+            // off the notecard when drawing is happening.
             if (touch.transform.name.Contains("Notecard"))
             {
                 whiteboard.transform.GetComponent<BoxCollider>().enabled = false;
@@ -115,6 +128,8 @@ public class Drawer : MonoBehaviour
                 return;
             }
 
+            // If the board was touched in the previous frame, and we are currently either 
+            // drawing or deleting, then begin setting pixels appropiately
             if (touchedLastFrame && (isDeleting || drawMode.action.ReadValue<float>() > 0.0f))
             {
                 whiteboard.SetPixels(x, y, penSize, penSize, colors);
@@ -124,6 +139,8 @@ public class Drawer : MonoBehaviour
                 {
                     var lerpX = (int)Mathf.Lerp(lastTouchPos.x, x, f);
                     var lerpY = (int)Mathf.Lerp(lastTouchPos.y, y, f);
+
+                    // When erasing, the pixel set is bigger so that it's not tedious to erase
                     if (isDeleting)
                     {
                         whiteboard.SetPixels(lerpX, lerpY, penSize * 15, penSize * 15, colors);
@@ -156,6 +173,8 @@ public class Drawer : MonoBehaviour
 
     }
 
+    // Check whether we're deleting or drawing. 
+    // This is done by checking whether the A or B button is being pressed.
     private void CheckDeleteState()
     {
         isDeleting = deleteMode.action.ReadValue<float>() > 0.0f;
@@ -169,6 +188,10 @@ public class Drawer : MonoBehaviour
         }
     }
 
+    // Create a line from the pen to the board by using a line renderer on a new 
+    // game object. The color holds no bearing at the moment, as the material of the line
+    // will always be green. After that, delete the line during this frame, as it can't move.
+    // The next frame, if the pen is still raycast hitting the board, then make another. 
     void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.01f)
     {
         GameObject myLine = new GameObject();
